7e430f04bdbea9a1811bd00cf81c59e26302b508
liuyuxia <1843353966@qq.com>
2021-04-14 15:53:01 +0800

    update

--- travel/src/App.vue
+++ travel/src/App.vue
@@ -1,23 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- 当前路由所对应的内容 -->
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {margin: 0;padding: 0;box-sizing: border-box;text-decoration: none;list-style: none;
 }
+body,html{width: 100%;height: 100%;}
+#app{width: 100%;height: 100%;}
 </style>
